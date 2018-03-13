efe99658369370b7185c7fb4962b2b292bee6442
Vishnu <vishnunarang2000@gmail.com>
2018-03-13 12:09:26 -0700

    Fixing parsing when not all questions are answered

--- modules/result_generator.py
+++ modules/result_generator.py
@@ -2,6 +2,21 @@ import xml.etree.ElementTree
 import csv
 import time
 import os
+import re
+
+
+def atoi(text):
+    return int(text) if text.isdigit() else text
+
+
+def natural_keys(text):
+    '''
+    alist.sort(key=natural_keys) sorts in human order
+    http://nedbatchelder.com/blog/200712/human_sorting.html
+    (See Toothy's implementation in the comments)
+    '''
+    return [atoi(c) for c in re.split('(\d+)', text)]
+
 
 def writeToCSV(destpath, results, keys):
     """ Creates a csv file and writes the data from the <results> dictionary. """
@@ -35,23 +50,43 @@ def parseXMLFile(filepath, index):
 
 
 def parse_questions(keys, root, row):
-    questions = root.findall(".//Interactions")
+    interactions = root.findall(".//Interactions")
+    variables = root.find(".//Variables")
+    ans_els = [elem.tag for elem in variables.iter() if elem is not variables]
+    ans_els.sort(key=natural_keys)
+
     total_correct = 0
     total_wrong = 0
+    total_skipped = 0
+
     keys.append('correct-count')
     keys.append('wrong-count')
-    for i, question in enumerate(questions):
+    keys.append('skipped')
+
+    interaction_counter = 0
+
+    for i, ans_el in enumerate(ans_els):
         qn = "Answer to Question %d" % (i)
-        row[qn] = question.find('StudentResponse').get('value')
         keys.append(qn)
-        # row['time-taken'] = question.find('Latency').get('value')
-        result = question.find('Result').get('value')
-        if result == 'W':
-            total_wrong += 1
-        elif result == 'C':
-            total_correct += 1
+
+        answer = variables.find(ans_el).get('value')
+        if answer != "":
+            if interaction_counter < len(interactions):
+                interaction = interactions[interaction_counter]
+                interaction_counter += 1
+                result = interaction.find("Result").get('value')
+                if result == 'W':
+                    total_wrong += 1
+                elif result == 'C':
+                    total_correct += 1
+            row[qn] = answer
+        else:
+            total_skipped += 1
+
     row['correct-count'] = total_correct
     row['wrong-count'] = total_wrong
+    row['skipped'] = total_skipped
+
 
 def generateResults(dirpath, destination_path):
     student_results = []
